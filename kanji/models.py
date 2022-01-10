37cc5fc276163504cd8244a25069b5418277547c
LeoKnox <LeoKnox@users.noreply.github.com>
2022-01-10 12:47:56 -0800

    Add files via upload

---
+++ kanji/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Kanji(models.Model):
+    kanji = models.CharField(max_length=100, blank=False)
+    strokes = models.IntegerField(default = 0)
+    meaning = models.CharField(max_length=100, blank=True)
+    pronounciation = models.CharField(max_length=100, blank=True)
+    reading = models.CharField(max_length=100, blank=True)
+    grade = models.IntegerField(default = 0)
