bb04b81e7a71d39aec764888f2250b5fa18bcb49
LeoKnox <LeoKnox@users.noreply.github.com>
2022-01-16 13:17:06 -0800

    Add files via upload

--- front_kanjichallenge/src/kanjis/kanjidetail.js
+++ front_kanjichallenge/src/kanjis/kanjidetail.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class KanjiDetail extends Component {
     render() {
+        const p = this.props.p
         return(
             <div>
                 <h4>{p.idkanji_dict}</h4>
