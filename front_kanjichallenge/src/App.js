527df299ffff0fc4c587d3a1649d06db63d61606
LeoKnox <LeoKnox@users.noreply.github.com>
2022-01-16 11:32:03 -0800

    Add files via upload

--- front_kanjichallenge/src/App.js
+++ front_kanjichallenge/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -5,18 +6,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://www.shareicon.net/data/256x256/2015/09/25/646362_japan_512x512.png" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Web App for learning Kanji
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Kanji Learn
+        </h1>
       </header>
     </div>
   );
