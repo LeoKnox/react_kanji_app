c678b1892f5f509965cf22337a0903efee74fd08
LeoKnox <LeoKnox@users.noreply.github.com>
2022-01-17 11:46:26 -0800

    Add files via upload

--- front_kanjichallenge/src/kanjis/kanjidetail.js
+++ front_kanjichallenge/src/kanjis/kanjidetail.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 
 class KanjiDetail extends Component {
     render() {
-        const p = this.props.p
+        const obj = this.props.kanjiDetail
         return(
-            <div>
-                <h4>{p.idkanji_dict}</h4>
-                <h4>{p.kanji}
-                : {p.meaning}</h4>
+            <div style={{ color: "yellow", border: "1px solid yellow"}}>
+                <h4>{obj.kanji}</h4>
+                <h5>Meaning: {obj.meaning} Pronounciation: {obj.pronounciation}</h5>
+                <h6>Reading: {obj.reading} Grade: {obj.grade} Strokes: {obj.strokes}</h6>
             </div>
-        )
+        );
     }
 }
 
--- front_kanjichallenge/src/kanjis/kanjislist.js
+++ front_kanjichallenge/src/kanjis/kanjislist.js
@@ -3,8 +3,31 @@ import axios from 'axios';
 import KanjiDetail from './kanjidetail';
 
 class KanjiList extends Component {
-    state = {
-        kanjisData:[],
+    constructor(props) {
+        super(props);
+        this.state = {
+            kanjisData: [],
+            kanji: "",
+            showComponent: false,
+        };
+        this.getKanjiDetail = this.getKanjiDetail.bind(this);
+        this.showKanjiDetails = this.showKanjiDetails.bind(this);
+    }
+
+    getKanjiDetail(item) {
+        axios
+            .get("http://127.0.0.1:8000".concat(item.absolute_url))
+            .then((response) => {
+                this.setState({ kanji: response.data });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    showKanjiDetails(item) {
+        this.getKanjiDetail(item);
+        this.setState({ showComponent: true });
     }
 
     componentDidMount() {
@@ -21,10 +44,18 @@ class KanjiList extends Component {
         return(
             <div>
                 {this.state.kanjisData.map( item => {
-                    return <h3>{item.kanji} : {item.meaning}</h3>
+                    return (
+                        <h3 key={item.id} onClick={() => this.showKanjiDetails(item)}>
+                            {item.kanji}: {item.meaning}
+                        </h3>
+                    );
                 })}
+
+                {this.state.showComponent ? (
+                    <KanjiDetail kanjiDetail={this.state.kanji} />
+                ) : null}
             </div>
-        )
+        );
     }
 }
 
