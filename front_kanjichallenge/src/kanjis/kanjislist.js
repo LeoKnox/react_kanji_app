3f487ba9d91b6b26b1478b4c908407b90986036c
LeoKnox <LeoKnox@users.noreply.github.com>
2022-01-17 11:14:17 -0800

    Add files via upload

--- front_kanjichallenge/src/kanjis/kanjislist.js
+++ front_kanjichallenge/src/kanjis/kanjislist.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-import DummyData from './dummydata.json';
+import axios from 'axios';
 import KanjiDetail from './kanjidetail';
 
 class KanjiList extends Component {
+    state = {
+        kanjisData:[],
+    }
+
+    componentDidMount() {
+        axios.get("http://127.0.0.1:8000/")
+        .then((response) => {
+            this.setState({kanjisData: response.data})
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    }
+
     render() {
         return(
             <div>
-                {DummyData.map( item => {
-                    return <KanjiDetail p = {item}/>
-                    })
-                }
+                {this.state.kanjisData.map( item => {
+                    return <h3>{item.kanji} : {item.meaning}</h3>
+                })}
             </div>
         )
     }
