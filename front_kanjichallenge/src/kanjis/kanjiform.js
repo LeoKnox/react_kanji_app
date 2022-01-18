5ef65c55be2755d5c2b873e51a588244e8b65b1f
LeoKnox <LeoKnox@users.noreply.github.com>
2022-01-18 11:28:29 -0800

    Add files via upload

--- front_kanjichallenge/src/kanjis/kanjiform.js
+++ front_kanjichallenge/src/kanjis/kanjiform.js
@@ -2,7 +2,28 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class KanjiForm extends React.Component {
-    //code
+    handleChange(event) {
+        //code
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+    }
+    render() {
+        const { kanji_name } = this.state;
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <input
+                        type="text"
+                        name="kanji_name"
+                        value={kanji_name}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
 }
 
 export default KanjiForm;
