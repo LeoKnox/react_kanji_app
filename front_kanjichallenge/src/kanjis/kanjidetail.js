a6f4b5d20c71c822323253201909e1c84c354ed3
LeoKnox <LeoKnox@users.noreply.github.com>
2022-01-19 11:06:56 -0800

    Add files via upload

--- front_kanjichallenge/src/kanjis/kanjidetail.js
+++ front_kanjichallenge/src/kanjis/kanjidetail.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 
 class KanjiDetail extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showComponent: false,
+        };
+        this.updateKanjiDetails = this.updateKanjiDetails.bind(this);
+    }
+    updateKanjiDetail() {
+        this.setState({ showComponent: true });
+    }
+
     render() {
         const obj = this.props.kanjiDetail
         return(
@@ -8,6 +19,12 @@ class KanjiDetail extends Component {
                 <h4>{obj.kanji}</h4>
                 <h5>Meaning: {obj.meaning} Pronounciation: {obj.pronounciation}</h5>
                 <h6>Reading: {obj.reading} Grade: {obj.grade} Strokes: {obj.strokes}</h6>
+                <button
+                    style={{ backgroundColor: "white" }}
+                    onClick={() => this.updateKanjiDetails()}>
+                    Update
+                </button>
+                {this.state.showComponent ? <KanjiUpdate kanjiupdate={obj} /> : null}
             </div>
         );
     }
