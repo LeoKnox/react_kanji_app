8438d8fc844395cb9eaaf3987230bdf0e4d10392
LeoKnox <LeoKnox@users.noreply.github.com>
2022-01-22 15:10:30 -0800

    Add files via upload

--- kanjivs_app/App.js
+++ kanjivs_app/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
 import ListView from "./src/screens/components/list_view";
+import DetailView from "./src/screens/components/detail_view";
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  const aboutapp = "learn and use kanji!"
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.baseText}>Kanji Challenge App</Text>
-      <Text style={styles.newText}>{aboutapp}</Text>
-      <ListView />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={ListView} />
+        <Stack.Screen name="Detail" component={DetailView} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
