2cb2c872b2c776248e2d0d467eed60bedb0a281c
LeoKnox <LeoKnox@users.noreply.github.com>
2022-01-22 14:53:28 -0800

    Add files via upload

--- kanjivs_app/src/screens/components/list_view.js
+++ kanjivs_app/src/screens/components/list_view.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
+import { StyleSheet, SafeAreaView, Text, Button } from "react-native";
 
 class Listview extends Component {
     render() {
+        const myText = "learn Kanji while I learning programming";
         return (
-            <View style={styles.center}>
+            <SafeAreaView style={styles.center}>
+                <Text style={styles.baseText}>Kanji Training App</Text>
+                <Text style={styles.newText}>{myText}</Text>
                 <Text style={styles.title}>List View</Text>
                 <Button title="list Item, Click for Details" />
-            </View>
+            </SafeAreaView>
         );
     }
 }
@@ -21,6 +24,14 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 36,
         marginBottom: 16,
+    },
+    baseText: {
+        color: "navy",
+        fontSize: 30,
+        fontStyle: "italic",
+    },
+    newText: {
+        color: "red",
     }
 });
 
