6606f45b9702bb9a297a7550581e5f52265f9069
LeoKnox <LeoKnox@users.noreply.github.com>
2022-01-22 14:40:22 -0800

    Add files via upload

---
+++ kanjivs_app/src/screens/components/detail_view.js
@@ -0,0 +1,26 @@
+import React, { Component } from  "react";
+import { StyleSheet, View, Text } from "react-native";
+
+class DetailView extends Component {
+    render() {
+        return (
+            <View style={StyleSheet.center}>
+                <Text style={StyleSheet.title}>Detail View</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    center: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    title: {
+        fontSize: 36,
+        marginBottom: 16,
+    },
+});
+
+export default DetailView;
