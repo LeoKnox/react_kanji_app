c203fd5c073c51e4e0a41c44f9555ac3deb106a3
LeoKnox <LeoKnox@users.noreply.github.com>
2022-01-22 14:27:28 -0800

    Add files via upload

--- kanjivs_app/src/screens/components/list_view.js
+++ kanjivs_app/src/screens/components/list_view.js
@@ -1,1 +1,27 @@
-a
+import React, { Component } from "react";
+import { StyleSheet, View, Text, Button } from "react-native";
+
+class Listview extends Component {
+    render() {
+        return (
+            <View style={styles.center}>
+                <Text style={styles.title}>List View</Text>
+                <Button title="list Item, Click for Details" />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    center: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    title: {
+        fontSize: 36,
+        marginBottom: 16,
+    }
+});
+
+export default Listview;
