0d7e3e7aa9298bdc37771c2ba054fdfe90880439
LeoKnox <LeoKnox@users.noreply.github.com>
2022-01-23 11:22:50 -0800

    Add files via upload

--- kanjivs_app/App.js
+++ kanjivs_app/App.js
@@ -2,19 +2,33 @@ import React from 'react';
 import { StyleSheet, Text, SafeAreaView } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import ListView from "./src/screens/components/list_view";
 import DetailView from "./src/screens/components/detail_view";
+import ScreenA from "./src/screens/drawer/screenA.js";
+import ScreenB from "./src/screens/drawer/screenB.js";
+import ScreenC from "./src/screens/drawer/screenC.js";
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+renderScreenComponents = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Home" component={ListView} />
+    <Sctrack.Screen name="Detail" component={DetailView} />
+  </Stack.Navigator>
+);
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={ListView} />
-        <Stack.Screen name="Detail" component={DetailView} />
-      </Stack.Navigator>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" children={this.renderScreenComponents} />
+        <Drawer.Screen name="Screen A" component={ScreenA} />
+        <Drawer.Screen name="Screen B" component={ScreenB} />
+        <Drawer.Screen name="Screen C" component={ScreenC} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
