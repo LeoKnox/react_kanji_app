67d992330b55828d3ff1a8320a0bebd9306bf96a
LeoKnox <LeoKnox@users.noreply.github.com>
2022-01-24 12:16:54 -0800

    Add files via upload

--- kanjivs_app/App.js
+++ kanjivs_app/App.js
@@ -3,21 +3,35 @@ import { StyleSheet, Text, SafeAreaView } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import ListView from "./src/screens/components/list_view";
 import DetailView from "./src/screens/components/detail_view";
 import ScreenA from "./src/screens/drawer/screenA.js";
 import ScreenB from "./src/screens/drawer/screenB.js";
 import ScreenC from "./src/screens/drawer/screenC.js";
+import TabOne from "./src/screens/tabs/tab1.js";
+import TabTwo from "./src/screens/tabs/tab2.js";
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
+
+const renderTabComponents = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Tab 1" component={TabOne} />
+      <Tab.Screen name="Tab 2" component={TabTwo} />
+    </Tab.Navigator>
+  );
+};
 
 const renderScreenComponents = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Home" component={ListView} />
       <Stack.Screen name="Detail" component={DetailView} />
+      <Stack.Screen name="Tabs" children={renderTabComponents} />
     </Stack.Navigator>
   );
 };
