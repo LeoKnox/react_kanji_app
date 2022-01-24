77199ce9f8aea96836e72e1467c0abffcefe130c
LeoKnox <LeoKnox@users.noreply.github.com>
2022-01-24 12:14:48 -0800

    Add files via upload

--- kanjivs_app/src/screens/components/detail_view.js
+++ kanjivs_app/src/screens/components/detail_view.js
@@ -1,13 +1,17 @@
 import React, { Component } from  "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 
 class DetailView extends Component {
     render() {
         return (
             <View style={styles.center}>
                 <Text style={styles.title}>Detail View</Text>
+                <button
+                    title="Click for Tabs"
+                    onPress={() => this.props.navigation.navigate("Tabs")}
+                />
             </View>
-        )
+        );
     }
 }
 
