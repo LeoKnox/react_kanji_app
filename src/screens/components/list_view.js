fd2e505337855934e98e4c7ced28c7b64189c83e
LeoKnox <LeoKnox@users.noreply.github.com>
2022-01-25 10:33:57 -0800

    Add files via upload

---
+++ src/screens/components/list_view.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { StyleSheet, SafeAreaView, Text, Button } from "react-native";
+
+class Listview extends Component {
+    render() {
+        const myText = "learn Kanji while I learning programming";
+        return (
+            <SafeAreaView style={styles.center}>
+                <Text style={styles.baseText}>Kanji Training App</Text>
+                <Text style={styles.newText}>{myText}</Text>
+                <Text style={styles.title}>List View</Text>
+                <Button
+                    title="list Item, Click for Details"
+                    onPress={() => this.props.navigation.navigate("Detail")}
+                />
+            </SafeAreaView>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    center: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    title: {
+        fontSize: 36,
+        marginBottom: 16,
+    },
+    baseText: {
+        color: "navy",
+        fontSize: 30,
+        fontStyle: "italic",
+    },
+    newText: {
+        color: "red",
+    }
+});
+
+export default Listview;
