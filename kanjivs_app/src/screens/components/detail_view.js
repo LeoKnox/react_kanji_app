3c4186ea5b2be1fa66eda5ffecae148a9167840c
LeoKnox <LeoKnox@users.noreply.github.com>
2022-01-25 10:55:32 -0800

    Add files via upload

--- kanjivs_app/src/screens/components/detail_view.js
+++ kanjivs_app/src/screens/components/detail_view.js
@@ -1,5 +1,5 @@
 import React, { Component } from  "react";
-import { StyleSheet, View, Text, Button } from "react-native";
+import { StyleSheet, View, Text, Button} from "react-native";
 
 class DetailView extends Component {
     render() {
--- kanjivs_app/src/screens/components/list_view.js
+++ kanjivs_app/src/screens/components/list_view.js
@@ -1,7 +1,21 @@
 import React, { Component } from "react";
-import { StyleSheet, SafeAreaView, Text, Button } from "react-native";
+import { StyleSheet, SafeAreaView, Text, Button, FlatList } from "react-native";
+import client from "./../../api/client";
 
 class Listview extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+        };
+    }
+
+    componentDidMount() {
+        client.get("/").then((response) => {
+            this.setState({ data: response.data });
+        });
+    }
+
     render() {
         const myText = "learn Kanji while I learning programming";
         return (
@@ -12,6 +26,15 @@ class Listview extends Component {
                 <Button
                     title="list Item, Click for Details"
                     onPress={() => this.props.navigation.navigate("Detail")}
+                />
+                <FlatList
+                    data={data}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({ item }) => (
+                        <Text style={styles.itemText}>
+                            {item.kanji}: {item.meaning}
+                        </Text>
+                    )}
                 />
             </SafeAreaView>
         );
@@ -35,6 +58,10 @@ const styles = StyleSheet.create({
     },
     newText: {
         color: "red",
+    },
+    itemText: {
+        color: "green",
+        fontSize: 20,
     }
 });
 
