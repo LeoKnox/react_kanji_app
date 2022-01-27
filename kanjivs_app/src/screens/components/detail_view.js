b3d20d6b6f09bf9c64cd2c6bae224eb3b16f1d40
LeoKnox <LeoKnox@users.noreply.github.com>
2022-01-27 10:40:36 -0800

    Add files via upload

--- kanjivs_app/src/screens/components/detail_view.js
+++ kanjivs_app/src/screens/components/detail_view.js
@@ -1,13 +1,34 @@
-import React, { Component } from  "react";
+import React, { useState, useEffect } from  "react";
 import { StyleSheet, View, Text, Button} from "react-native";
+import client from "./../../api/client";
 
 const DetailView = ({ navigation, route }) => {
+    const [detail, setDetail]= useState("");
     const { objurl } = route.params;
-    const { hey } = route.params;
+
+    const getDetail = async (url) => {
+        try {
+            const response = await client.get(url);
+            if (!response.ok) {
+                setDetail(response.data);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect (() => {
+        getDetail(objurl);
+    }, [])
+
     return (
         <View style={styles.center}>
-            <Text style={styles.title}>{hey}</Text>
-            <Text style={styles.title}>{objurl}</Text>
+            <Text style={styles.title}>Kanji: {detail.kanji}</Text>
+            <Text style={styles.title}>Pronounciation: {detail.pronounciation}</Text>
+            <Text style={styles.title}>Meaning: {detail.meaning}</Text>
+            <Text style={styles.title}>Reading: {detail.reading}</Text>
+            <Text style={styles.title}>Strokes: {detail.strokes}</Text>
+            <Text style={styles.title}>Grade: {detail.grade}</Text>
         </View>
     );
 };
